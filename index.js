2a6e8729c42fa227405401e17fca015386904942
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-11-15 17:59:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const sha256 = require('crypto-js/sha256.js');
 const express = require('express');
+const sha256 = require('crypto-js/sha256.js');
 const app = express();
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 class Block {
@@ -22,7 +22,7 @@ class Block {
         return sha256(
             this.index +
             this.precedingHash + 
-            this.timestamp +
+            this.timestamp + 
             JSON.stringify(this.transaction)
         ).toString();
     }
@@ -39,14 +39,14 @@ class BlockChain {
     create(id, name, genesis) {
         this.id = id;
         this.name = name;
-        this.blockchain = [this.startGenesisBlock(genesis)];
+        this.blockchain = [this.startGenesisblock(genesis)];
         this.difficulty = 4;
     }
 
-    startGenesisBlock(genesis) {
+    startGenesisblock(genesis) {
         return new Block(
-            0, 
-            genesis.date, 
+            0,
+            genesis.date,
             genesis.transaction,
             "0"
         );
@@ -70,7 +70,7 @@ class BlockChain {
             if (currentBlock.hash !== currentBlock.computeHash()) {
                 return false;
             }
-            
+
             if (currentBlock.precedingHash !== precedingBlock.hash) {
                 return false;
             }
@@ -88,8 +88,8 @@ class MilerCoin {
 
     validateNewChain = (req, res, next) => {
         if (req.body) {
-            if (req.body.id && 
-                req.body.name && 
+            if (req.body.id &&
+                req.body.name &&
                 req.body.genesis &&
                 req.body.genesis.date &&
                 req.body.genesis.transaction
@@ -99,18 +99,17 @@ class MilerCoin {
                 res.status(400).json({ message: 'Request format is not correct!'});
             }
         } else {
-            res.status(400).json({ message: 'Request format is not correct!'})
+            res.status(400).json({ message: 'Request format is not correct!'});
         }
     }
 
     createNewChain = (req, res) => {
-        const chain  = GlobalChain.create(
+        const block = GlobalChain.create(
             req.body.id,
             req.body.name,
-            req.body.genesis
+            req.body.genesis,
         )
-
-        res.status(200).json({ message: 'Chain created', data: GlobalChain })
+        res.status(200).json({ message: 'Chain created!', data: GlobalChain });
     }
 
     appendNewChild = (req, res) => {
@@ -120,20 +119,23 @@ class MilerCoin {
             req.body.transaction
         )
         GlobalChain.addNewBlock(block);
-        res.status(200).json({ message: 'Block added!'})
+        res.status(200).json({ message: 'Block added!'});
     }
 
     getChain = (req, res) => {
-        res.status(200).json({ chain: GlobalChain })
+        res.status(200).json({ chain: GlobalChain });
     }
 }
 
 const Controller = new MilerCoin();
 
-app.post('/blockchain', Controller.validateNewChain, Controller.createNewChain);
-app.get('/blockchain/', Controller.getChain);
-app.post('/blockchain/append', Controller.appendNewChild);
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Welcome to my blockchain'});
+});
+app.post('/api/blockchain', Controller.validateNewChain, Controller.createNewChain);
+app.get('/api/blockchain', Controller.getChain);
+app.post('/api/blockchain/append', Controller.appendNewChild);
 
 app.listen(9090, () => {
-    console.log('App starting on port 9090');
-});
+    console.log('Your blockchain is running!');
+})
