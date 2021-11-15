4cc5c4487ff971e77ed6d96291ea153535c9add0
Patiphan Phengpao <misterpatipan123@gmail.com>
2021-11-15 13:35:10 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,139 @@
+const sha256 = require('crypto-js/sha256.js');
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+
+class Block {
+    constructor(
+        index,
+        timestamp,
+        transaction,
+        precedingHash = ''
+    ) {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.transaction = transaction;
+        this.precedingHash = precedingHash;
+        this.hash = this.computeHash();
+    }
+
+    computeHash() {
+        return sha256(
+            this.index +
+            this.precedingHash + 
+            this.timestamp +
+            JSON.stringify(this.transaction)
+        ).toString();
+    }
+}
+
+class BlockChain {
+    constructor() {
+        this.id = '';
+        this.name = '';
+        this.blockchain = '';
+        this.difficulty = '';
+    }
+
+    create(id, name, genesis) {
+        this.id = id;
+        this.name = name;
+        this.blockchain = [this.startGenesisBlock(genesis)];
+        this.difficulty = 4;
+    }
+
+    startGenesisBlock(genesis) {
+        return new Block(
+            0, 
+            genesis.date, 
+            genesis.transaction,
+            "0"
+        );
+    }
+
+    obtainLatestBlock() {
+        return this.blockchain[this.blockchain.length - 1];
+    }
+
+    addNewBlock(newBlock) {
+        newBlock.precedingHash = this.obtainLatestBlock().hash;
+        newBlock.hash = newBlock.computeHash();
+        this.blockchain.push(newBlock);
+    }
+
+    checkChainValidity() {
+        for (let i = 1; i < this.blockchain.length; i++) {
+            const currentBlock = this.blockchain[i];
+            const precedingBlock = this.blockchain[i - 1];
+
+            if (currentBlock.hash !== currentBlock.computeHash()) {
+                return false;
+            }
+            
+            if (currentBlock.precedingHash !== precedingBlock.hash) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+const GlobalChain = new BlockChain();
+
+class MilerCoin {
+    constructor() {
+        this.chain = [];
+    }
+
+    validateNewChain = (req, res, next) => {
+        if (req.body) {
+            if (req.body.id && 
+                req.body.name && 
+                req.body.genesis &&
+                req.body.genesis.date &&
+                req.body.genesis.transaction
+                ) {
+                next();
+            } else {
+                res.status(400).json({ message: 'Request format is not correct!'});
+            }
+        } else {
+            res.status(400).json({ message: 'Request format is not correct!'})
+        }
+    }
+
+    createNewChain = (req, res) => {
+        const chain  = GlobalChain.create(
+            req.body.id,
+            req.body.name,
+            req.body.genesis
+        )
+
+        res.status(200).json({ message: 'Chain created', data: GlobalChain })
+    }
+
+    appendNewChild = (req, res) => {
+        const block = new Block(
+            this.chain.length,
+            req.body.timestamp,
+            req.body.transaction
+        )
+        GlobalChain.addNewBlock(block);
+        res.status(200).json({ message: 'Block added!'})
+    }
+
+    getChain = (req, res) => {
+        res.status(200).json({ chain: GlobalChain })
+    }
+}
+
+const Controller = new MilerCoin();
+
+app.post('/blockchain', Controller.validateNewChain, Controller.createNewChain);
+app.get('/blockchain/', Controller.getChain);
+app.post('/blockchain/append', Controller.appendNewChild);
+
+app.listen(9090, () => {
+    console.log('App starting on port 9090');
+});
